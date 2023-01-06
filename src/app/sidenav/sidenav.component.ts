63c656c3901cfb891006f541343842e39361d20f
rachnakafle <rachanakafle99@gmail.com>
2023-01-06 14:04:34 +0545

    Animated Sidemenu added

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,19 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
 import { navbarData } from './nav-data';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
-
-  constructor() { }
+  @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
 
   collapsed = false;
+  screenWidth = 0;
   navData = navbarData;
 
-  ngOnInit(): void {
+  ngOnInit():void{
+    this.screenWidth = window.innerWidth;
   }
 
+  constructor() {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any){
+    this.screenWidth = window.innerWidth;
+    if(this.screenWidth <= 768){
+      this.collapsed = false;
+      this.onToggleSideNav.emit({
+        collapsed: this.collapsed,
+        screenWidth: this.screenWidth,
+      });
+    }
+  }
+
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
+
+  closeSidenav(): void {
+    this.collapsed = false;
+
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
 }

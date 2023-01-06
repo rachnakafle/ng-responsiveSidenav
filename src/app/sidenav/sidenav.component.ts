554e07b3c1911bf3635ed89b508288b3caac3bed
rachnakafle <rachanakafle99@gmail.com>
2023-01-06 15:32:14 +0545

    Added animations on cross icon

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,4 +1,17 @@
-import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
+import {
+  animate,
+  keyframes,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import {
+  Component,
+  EventEmitter,
+  HostListener,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { navbarData } from './nav-data';
 
 interface SideNavToggle {
@@ -10,6 +23,29 @@ interface SideNavToggle {
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('350ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('350ms', style({ opacity: 0 })),
+      ]),
+    ]),
+    trigger('rotate', [
+      transition(':enter', [
+        animate(
+          '1000ms',
+          keyframes([
+            style({ transform: 'rotate(0deg)', offset: '0' }),
+            style({ transform: 'rotate(2turn)', offset: '1' }),
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
 export class SidenavComponent implements OnInit {
   @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
@@ -18,16 +54,16 @@ export class SidenavComponent implements OnInit {
   screenWidth = 0;
   navData = navbarData;
 
-  ngOnInit():void{
+  ngOnInit(): void {
     this.screenWidth = window.innerWidth;
   }
 
   constructor() {}
 
   @HostListener('window:resize', ['$event'])
-  onResize(event:any){
+  onResize(event: any) {
     this.screenWidth = window.innerWidth;
-    if(this.screenWidth <= 768){
+    if (this.screenWidth <= 768) {
       this.collapsed = false;
       this.onToggleSideNav.emit({
         collapsed: this.collapsed,
